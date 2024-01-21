61aca2399a768ef2ab0104218ff6eb0171eff2a0
sandwipshanto <moloyswe20@gmail.com>
2024-01-21 16:02:26 +0600

    GitHub upload

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:uispeed_grocery_shop/screens/login_screen.dart';
+import 'page/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
+}
